8889d6641dcc1e29d7a65f4fa02383389341818f
duongtrang2701 <duong1997trang@gmail.com>
2019-07-09 08:36:53 +0700

    Update layout

--- blog/app/models/post.rb
+++ blog/app/models/post.rb
@@ -4,7 +4,5 @@ class Post < ApplicationRecord
     has_many :comments, dependent: :destroy
     validates :title, :description, :image, presence: true
     validates :title, uniqueness: true
-    validates :image, allow_blank: true, format: {
-              with: %r{\.(gif|jpg|png)\Z}i,
-              message: 'Must be a URL for GIF, JPG or PNG image.'}
+
 end
