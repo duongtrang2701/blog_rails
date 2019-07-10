33000365686b9ae8939e8f0fdd22b347f5d72d05
duongtrang2701 <duong1997trang@gmail.com>
2019-07-10 12:45:23 +0700

    Upload image

--- blog/app/models/post.rb
+++ blog/app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
     #auto log model & id
     is_impressionable
     has_many :comments, dependent: :destroy
+    has_one_attached :image
     validates :title, :description, :image, presence: true
     validates :title, uniqueness: true
 end
