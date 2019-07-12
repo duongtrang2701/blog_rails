8a0faf6472e36856f8df2d8dc0c9e4ad7f1c0b7b
duongtrang2701 <duong1997trang@gmail.com>
2019-07-12 08:44:12 +0700

    Update blog

--- blog/app/controllers/application_controller.rb
+++ blog/app/controllers/application_controller.rb
@@ -1,10 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authorize
-
+  # def current_user
+  #   return unless session[:user_id]
+  #   @current_user ||= User.find(session[:user_id])
+  # end
   protected
-  def authorize
-    unless User.find_by(id: session[:user_id])
-      redirect_to login_url, notice: "Please log in"
+    def authorize
+      unless User.find_by(id: session[:user_id])
+        redirect_to login_url, notice: "Please log in"
+      end
     end
-  end
 end
--- blog/app/controllers/comments_controller.rb
+++ blog/app/controllers/comments_controller.rb
@@ -2,8 +2,7 @@ class CommentsController < ApplicationController
   skip_before_action :authorize, only: [:destroy, :create]
   def create
     @post = Post.find(params[:post_id])
-    @comment = @post.comments.create(params[:comment].permit(:name, :comment, :user_id))
- 
+    @comment = @post.comments.create(params[:comment].permit(:comment))
     respond_to do |format|
       format.html { redirect_to post_path(@post) }
       format.js 
